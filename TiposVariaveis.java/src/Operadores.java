01608d8e140b480a2538c872b18f29d4c3f028dd
guivnh <guivnh@gmail.com>
2023-03-02 15:26:15 -0300

    terceira alteracao que mostra soma e concatencao

---
+++ TiposVariaveis.java/src/Operadores.java
@@ -0,0 +1,35 @@
+public class Operadores {
+     public static void main(String[] args) {
+
+      /*  //Palavras concatenadas. Usando +
+        String nomeCompleto = "Linguagem" + " " + "Java";
+        
+        System.out.println(nomeCompleto);
+    } */
+//classe Operadores.java
+String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+//qual o resultado das expressoes abaixo?
+String concatenacao ="?"; 
+
+concatenacao = 1+1+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+1+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+(1+1+1);
+
+     }
+
+}
