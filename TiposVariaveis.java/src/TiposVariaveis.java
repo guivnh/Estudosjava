2030c7a7712b06c5c028e3bf593f159fd772ea4d
guivnh <guivnh@gmail.com>
2023-03-02 11:37:55 -0300

    alteracao mostrando algumas atribuicoes

--- TiposVariaveis.java/src/TiposVariaveis.java
+++ TiposVariaveis.java/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
+import java.util.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    String meuNome = "Guilherme Vanhoni";
+    char sexo = 'M';
+    int idade = 39;
+    boolean doaorgaos = false;
+    Date dataNascimento = new Date();
+
 }
